e18eeeb25e99ed6299115ee05b7eb199b6bab3f7
Héctor Pizano <hecto@LENOVO>
2021-02-13 14:48:36 -0600

    Adicion del modelo

---
+++ Data/RazorPagesMoveContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMove.Models;
+
+namespace RazorPageMove.Data
+{
+    public class RazorPagesMoveContext : DbContext
+    {
+        public RazorPagesMoveContext (DbContextOptions<RazorPagesMoveContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPageMove.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPageMove.Data;
+using RazorPageMove.Models;
+
+namespace RazorPageMove.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPageMove.Data.RazorPagesMoveContext _context;
+
+        public CreateModel(RazorPageMove.Data.RazorPagesMoveContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
